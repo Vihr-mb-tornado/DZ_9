e184b0aa14184c1564995a600ff4b7d3edc1fccb
Laptop <yevhen003@gmail.com>
2024-10-08 20:20:55 +0200

    все

--- main.py
+++ main.py
@@ -1,25 +1,62 @@
 ﻿import requests
-response = requests.get("https://whenisthenextmcufilm.com/api")
-print(response)
-print('\n')
-if response.ok:
-    # print(response.text)
-    print('\n\n')
-    film = response.json()
-    print('When is the next MCU film?')
-    print('\n')
-    print(film['title'] + ' releases in')
-    print(str(film['days_until']) + ' days!')
-    print('Release Date: ' + film['release_date'])
-    print('Production Type: ' + film['type'])
-    print('What\'s afterwards? ' + film['following_production']['title'])
+import random
 
-    print('\n\n')
-    print(film['overview'])
+#7438
+page = random.randint(1, 7438)
+#page = 2
+pageSize = 1
+url = f'https://api.disneyapi.dev/character?pageSize={pageSize}&page={page}'
+response = requests.get(url)
+# response = requests.get("https://api.disneyapi.dev/character")
+
+#print(response)
+#print('\n')
+
+if response.ok:
+    character = response.json()['data']
+
+    print(f'Персонаж: {character["name"]}')
+
+    s = character['films']
+    if len(s) > 0 :
+        print('Фільми:')
+    for film in s:
+        print(f'     {film}')
+
+    s = character['shortFilms']
+    if len(s) > 0:
+        print('Короткометражки:')
+    for film in s:
+        print(f'     {film}')
+
+    s = character['tvShows']
+    if len(s) > 0:
+        print('ТВ шоу:')
+    for film in s:
+        print(f'     {film}')
+
+    s = character['videoGames']
+    if len(s) > 0:
+        print('Відеоігри:')
+    for film in s:
+        print(f'     {film}')
+
+    s = character['parkAttractions']
+    if len(s) > 0:
+        print('Атракціони:')
+    for film in s:
+        print(f'     {film}')
 
 else:
+    print('Щось пішло не так...')
     print(f'{response.status.code}')
 
+'''коротометражок, серіалів та ігор,'''
+'''
+shortFilms
+tvShows
+videoGames
+parkAttractions
 '''
 
-'''
+
